f4553bfe27e0ca787b28803b1db090020b579a3a
Hitexa8 <132060945+Hitexa8@users.noreply.github.com>
2024-06-05 10:37:02 +0530

    Update MiddleWare.js

--- MiddleWare.js
+++ MiddleWare.js
@@ -1,6 +1,6 @@
-//Method 1
 var expr = require("express");
 var app = expr();
+//**********Method 1*************
 
 // middle 1
 const cb=(req,res,next)=>
@@ -25,3 +25,22 @@ app.get('/ee',(req,res)=>{
     res.send();
 })
 app.listen(5258)
+
+//**********Method 2*************
+app.use("/",(req, res, next) => {
+    console.log("request received on"+new Date());
+    next();
+    },
+    (req, res, next) => {
+    res.set("content-type","text/html")
+    res.write("Hello");
+    next();
+    },
+    (req, res) => {
+    res.write(`<div>
+    <h2>Welcome to LJU</h2>
+    <h5>Tutorial on Middleware</h5>
+    </div>`);
+    res.send();
+    });
+    app.listen(6001)
