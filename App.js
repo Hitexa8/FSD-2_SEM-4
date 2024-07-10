95505e5c43fe8c5bbb90d3ce8b9d4ee1875d2a93
Hitexa8 <132060945+Hitexa8@users.noreply.github.com>
2024-07-10 10:29:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
-import Task from './Task'
+// import Task from './Task'
+// function App() {
+//   return (
+//     <div>
+//       <Task/>
+//     </div>
+//   );
+// }
+import Pre from './Pre'
+// function App(){
+//   const xyz = 'computer'
+//   return(
+//     <div>
+//       <Pre Name={xyz} Price='70000'/>
+//       <Pre Name="Mobile" Price='30000'/>
+//     </div>
+//   )
+// }
+// export default App;
+
+// function App() {
+//     const details = {student_name:"ABC",University_name:"LJU"}
+//     return(
+//       <div>
+//         <Pre data = {details}></Pre>
+//       </div>
+//     )
+// }
+// export default App;
+
+// import Event from './Event1'
+// function App() {
+//   return (
+//     <div>
+//       <Event/>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+// import Form from './Form1'
+// function App() {
+//   return (
+//     <div>
+//       <Form/>
+//     </div>
+//   );
+// }
+// export default App;
+
+import Event from './Event2'
 function App() {
   return (
-    <div>
-      <Task/>
+    <div> 
+      <Event/>
     </div>
   );
 }
-
 export default App;
